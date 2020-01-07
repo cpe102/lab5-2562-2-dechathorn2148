27a6870e5827166f844a4331972f1a431ac2a239
lab314-16@labeng.local <lab314-16@labeng.local>
2020-01-07 16:19:20 +0700

    Lab5_2 Edit(1)

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,10 +1,30 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
+string mixText(string a,string b)
+{
+	int L1 = a.size();
+	int L2 = b.size();
+	int i = 0;
+	if(L1 != L2)
+	{
+		return "E";
+	}
+	string result = "";
+	while(i < L1)
+	{
+		result = result + a[i] + b[i];
+		i++;
+	}
+	return result;
+	
+}
 
 
 
-int main(){	
+int main()
+{	
 	cout << mixText("AAA","BBB") << "\n";
 	cout << mixText("Hello","World") << "\n";
 	cout << mixText("SOTUS","CHEER") << "\n";
@@ -12,6 +32,5 @@ int main(){
 	cout << mixText("6","9") << "\n";
 	cout << mixText("XXXXXXX","YYY") << "\n";	
 	cout << mixText("Y","XX") << "\n";	
-	
 	return 0;	
 }
